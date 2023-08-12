1d991350f3bea3a1855180165eace7e8c46b0d6c
Ogollah <kogollah2011@gmail.com>
2023-08-12 17:28:41 +0300

    :construction: Console

--- console.py
+++ console.py
@@ -1,8 +1,26 @@
+#!/usr/bin/env python3
 import cmd
-from models import *
-from models.engines import FileStorage
+"""
+HBNBCommand class.
+"""
+
 
 class HBNBCommand(cmd.Cmd):
-    print("implementation")
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit the command interpreter"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the command interpreter using Ctrl-D (EOF)"""
+        print()
+        return True
+
+    def emptyline(self):
+        """Do nothing when an empty line is entered"""
+        pass
+
+
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
